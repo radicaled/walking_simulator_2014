9291e84055a13f15e0b86680e43096ad9637a314
Arron Washington <arron@theradicaledwards.com>
2014-09-11 04:08:05 -0400

    Players no longer moonwalk.

--- lib/game/sprites/player_sprite.dart
+++ lib/game/sprites/player_sprite.dart
@@ -1,10 +1,22 @@
 import 'package:stagexl/stagexl.dart';
+import 'dart:collection';
 
-class PlayerSprite extends Bitmap {
+class PlayerSprite extends Bitmap implements Animatable {
   SpriteSheet spriteSheet;
+
+
+  static List upFrames = [0, 1, 2];
+  static List leftFrames = [18, 19, 20];
+  static List rightFrames = [6, 7, 8];
+  static List downFrames = [12, 13, 14];
+
+  Queue animationFrames;
 
   PlayerSprite(this.spriteSheet) : super(new BitmapData(24, 32)) {
     _update(13);
+    onAddedToStage.first.then((_) {
+      _animate(downFrames);
+    });
   }
 
   void _update(frameNo) {
@@ -21,23 +33,44 @@ class PlayerSprite extends Bitmap {
     this.stage.juggler.add(tween);
   }
 
+  _animate(List frames) {
+    animationFrames = new Queue.from(frames);
+    stage.juggler.add(this);
+  }
+
   void moveLeft() {
     _update(20);
     _animateXY(xDelta: -32);
+    _animate(leftFrames);
   }
 
   void moveRight() {
     _update(7);
     _animateXY(xDelta: 32);
+    _animate(rightFrames);
   }
 
   void moveUp() {
     _update(1);
     _animateXY(yDelta: -32);
+    _animate(upFrames);
   }
 
   void moveDown() {
     _update(13);
     _animateXY(yDelta: 32);
+    _animate(downFrames);
+  }
+
+  num _totalTime = 0.0;
+  bool advanceTime(num delta) {
+    _totalTime += delta;
+    if (_totalTime > 0.5) {
+      _totalTime = 0.0;
+      var nextFrame = animationFrames.removeFirst();
+      _update(nextFrame);
+      animationFrames.add(nextFrame);
+    }
+    return animationFrames.isNotEmpty;
   }
 }
